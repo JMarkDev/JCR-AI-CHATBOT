e20e0019b3d80bae1418b11714b5788240969cbc
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2025-01-28 01:35:26 +0800

    login done

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,9 +1,58 @@
 import logo from "../../assets/ai_logo.jpeg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import api from "../../api/axios";
+import LoginLoading from "../../components/loader/login_loader/Loader";
+import { toast, ToastContainer } from "react-toastify";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setEmailError("");
+    setPasswordError("");
+    setLoading(true);
+
+    try {
+      const response = await api.post("/auth/login", data);
+      if (response.data.status === "success") {
+        navigate("/chat");
+        toast.success(response.data.message);
+        setLoading(false);
+      }
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.response.data.message);
+      if (error.response.data.errors) {
+        error.response.data.errors.forEach((error) => {
+          switch (error.path) {
+            case "email":
+              setEmailError(error.msg);
+              break;
+            case "password":
+              setPasswordError(error.msg);
+              break;
+            default:
+              toast.error(error.message);
+          }
+        });
+      }
+    }
+  };
   return (
     <div className="min-h-screen px-5 bg-gradient-to-br from-blue-100 via-blue-300 to-blue-400 flex flex-col items-center justify-center">
+      {loading && <LoginLoading />}
+      <ToastContainer />
       <div className="flex flex-col w-full rounded-lg bg-white shadow-2xl justify-between items-center max-w-4xl mx-auto ">
         <div className="flex md:flex-row flex-col items-center w-full">
           {/* Form Section */}
@@ -14,7 +63,10 @@ const Login = () => {
             <p className="text-gray-500 text-sm mt-2">
               Please sign in to continue
             </p>
-            <form className="mt-6 w-full text-gray-600">
+            <form
+              onSubmit={handleSubmit}
+              className="mt-6 w-full text-sm text-gray-600"
+            >
               <div className="flex flex-col text-md mb-4">
                 <label htmlFor="email" className="text-sm font-semibold">
                   Email
@@ -22,9 +74,17 @@ const Login = () => {
                 <input
                   type="text"
                   id="email"
-                  className="p-3 rounded-lg bg-gray-100 border border-gray-300 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 shadow-sm hover:shadow-md transition-all"
+                  onChange={(e) => setData({ ...data, email: e.target.value })}
+                  className={`${
+                    emailError ? "border-red-500" : "border-gray-300"
+                  } p-3 rounded-lg bg-gray-100 border  focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 shadow-sm hover:shadow-md transition-all`}
                   placeholder="Enter your email"
                 />
+                {emailError && (
+                  <span className="text-red-500 text-sm">
+                    {emailError && emailError}
+                  </span>
+                )}
               </div>
               <div className="flex flex-col text-md">
                 <label htmlFor="password" className="text-sm font-semibold">
@@ -33,14 +93,31 @@ const Login = () => {
                 <input
                   type="password"
                   id="password"
-                  className="p-3 rounded-lg bg-gray-100 border border-gray-300 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 shadow-sm hover:shadow-md transition-all"
+                  onChange={(e) =>
+                    setData({ ...data, password: e.target.value })
+                  }
+                  className={` ${
+                    passwordError ? "border-red-500" : "border-gray-300"
+                  } p-3 rounded-lg bg-gray-100 border  focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 shadow-sm hover:shadow-md transition-all`}
                   placeholder="Enter your password"
                 />
+                {passwordError && (
+                  <span className="text-red-500 text-sm">
+                    {passwordError && passwordError}
+                  </span>
+                )}
               </div>
+              <p className="text-sm text-right mt-2">
+                <Link to={"/forgot-password"} className="text-blue-500 mt-2">
+                  Forgot Password?
+                </Link>
+              </p>
 
               <button
                 type="submit"
-                className="mt-6 w-full p-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 shadow-md hover:shadow-lg transition-all"
+                disabled={loading}
+                className={`${loading ? "cursor-not-allowed" : "cursor-pointer"}
+                  mt-6 w-full p-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 shadow-md hover:shadow-lg transition-all`}
               >
                 Login
               </button>
